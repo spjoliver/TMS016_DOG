5eb605807a24bcbaa9924892304ecab09ce36774
Oliver Spjuth <oliver.spjuth@gmail.com>
2023-04-19 08:02:45 +0000

    Added som dosctrings and type hints to matlab functions.

--- Part 1/matlab_functions.py
+++ Part 1/matlab_functions.py
@@ -1,32 +1,74 @@
 from scipy.special import kv, betainc
 from scipy.special import gammaln as lgamma
 from scipy import sparse
-from scipy.optimize import fmin, fmin_tnc
+from scipy.optimize import minimize
 import numpy as np
 from scipy.spatial.distance import squareform, pdist
 from matplotlib.colors import LinearSegmentedColormap
-from scipy.sparse import linalg as splinalg
-import sys
+from typing import Dict, Tuple, Optional, Union, Callable, List
 
-def emp_variogram(D, data, N):
+def emp_variogram(D: np.ndarray, data: np.ndarray, N: int) -> Dict[str, np.ndarray]:
+    """
+    Calculate the empirical variogram from input data and distance matrix.
+    
+    Parameters
+    ----------
+    D : numpy.ndarray
+        Input distance matrix or coordinates array.
+    data : numpy.ndarray
+        Input data values corresponding to each point in D.
+    N : int
+        Number of bins for calculating the variogram.
+    
+    Returns
+    -------
+    out : dict
+        Dictionary containing the variogram information (bin centers, variogram values, and number of pairs per bin).
+    """
+    
+    # Ensure that D is a square matrix. If not, convert it using pdist and squareform from scipy.
     if D.shape[0] != D.shape[1]:
         D = squareform(pdist(D))
+    
     max_dist = D.max()
+    
+    # Create an array of linearly spaced distances from 0 to max_dist with N elements.
     d = np.linspace(0, max_dist, N)
+    
+    # Set the lower triangle of the distance matrix to -1 to avoid duplicate calculations.
     D[np.tril_indices(D.shape[0], k=-1)] = -1
     
     out = {}
+    # Calculate bin centers.
     out['h'] = (d[1:] + d[:-1]) / 2
     out['variogram'] = np.zeros(N-1)
     out['N'] = np.zeros(N-1)
     
+    # Calculate the empirical variogram.
     for i in range(N-1):
         I, J = np.where((d[i] < D) & (D <= d[i+1]))
         out['N'][i] = len(I)
         out['variogram'][i] = 0.5 * np.mean((data[I] - data[J])**2)
     return out
 
-def stencil2prec(sz, q):
+
+def stencil2prec(sz: Tuple[int, int], q: np.ndarray) -> sparse.csc_matrix:
+    """
+    Create a precision matrix from a stencil matrix.
+    
+    Parameters
+    ----------
+    sz : tuple of int
+        Tuple containing the dimensions of the desired precision matrix.
+    q : numpy.ndarray
+        Stencil matrix used to create the precision matrix.
+    
+    Returns
+    -------
+    Q : scipy.sparse.csc_matrix
+        The resulting precision matrix in Compressed Sparse Column (CSC) format.
+    """
+
     II = []
     KK = []
     JJ_I = []
@@ -36,25 +78,30 @@ def stencil2prec(sz, q):
     I = I.flatten()
     J = J.flatten()
 
-    ones_arr = np.ones((sz[0]*sz[1], 1))
+    ones_arr = np.ones((sz[0] * sz[1], 1))
+
+    # Loop over the stencil matrix elements to compute II, JJ_I, JJ_J, and KK lists.
     for i in range(1, 1 + q.shape[0]):
         for j in range(1, 1 + q.shape[1]):
-            if q[i-1,j-1] != 0:
-                II.append(I + sz[0]*(J-1))
-                JJ_I.append(I + i - int((q.shape[0]+1)/2))
-                JJ_J.append(J + j - int((q.shape[1]+1)/2))
-                KK.append(q[i-1,j-1] * ones_arr)
+            if q[i - 1, j - 1] != 0:
+                II.append(I + sz[0] * (J - 1))
+                JJ_I.append(I + i - int((q.shape[0] + 1) / 2))
+                JJ_J.append(J + j - int((q.shape[1] + 1) / 2))
+                KK.append(q[i - 1, j - 1] * ones_arr)
 
-    II = np.concatenate(II)[:, np.newaxis]
-    JJ_I = np.concatenate(JJ_I)[:, np.newaxis]
-    JJ_J = np.concatenate(JJ_J)[:, np.newaxis]
+    II = np.concatenate(II)
+    JJ_I = np.concatenate(JJ_I)
+    JJ_J = np.concatenate(JJ_J)
     KK = np.concatenate(KK)
-    JJ = JJ_I + sz[0]*(JJ_J-1)
+    JJ = JJ_I + sz[0] * (JJ_J - 1)
+
+    # Filter out indices that are out of bounds.
     ok = (JJ_I >= 1) & (JJ_I <= sz[0]) & (JJ_J >= 1) & (JJ_J <= sz[1])
-    II = II[ok]
-    JJ = JJ[ok]
-    KK = KK[ok]
-    Q = sparse.coo_matrix((KK, (II-1, JJ-1)), shape=(np.prod(sz), np.prod(sz)))
+
+    # Create a sparse matrix in COO format using the filtered data.
+    Q = sparse.coo_matrix((KK[ok], (II[ok] - 1, JJ[ok] - 1)), shape=(np.prod(sz), np.prod(sz)))
+
+    # Convert the sparse matrix to the CSC format.
     return Q.tocsc()
 
 def matern_covariance(h, sigma, kappa, nu):
@@ -133,34 +180,79 @@ def matern_variogram(h, sigma, kappa, nu, sigma_e=0):
     return sv
 
 
-def log_like(p, covf, data, X, D, I, fixed, n_cov, names, reml):
-    #compute covariance matrix 
-    if names[-1] == 'sigma_e': # nugget not fixed
-        Sigma = covf(D,np.exp(p[:-1])) + np.exp(2*p[-1])*I
-    else:
-        Sigma = covf(D,np.exp(p)) + (fixed['sigma_e'])**2*I
+def log_like(
+    p: np.ndarray,
+    covf: Callable,
+    data: np.ndarray,
+    X: np.ndarray,
+    D: np.ndarray,
+    I: np.ndarray,
+    fixed: Dict[str, float],
+    n_cov: int,
+    names: List[str],
+    reml: bool
+) -> float:
+    """
+    Log likelihood function for covariance parameter estimation.
     
-    #compute Cholesky factor, if it fails, return large value
-    R,p = np.linalg.cholesky(Sigma)
+    Parameters
+    ----------
+    p : numpy.ndarray
+        Parameters to estimate.
+    covf : callable
+        Covariance function.
+    data : numpy.ndarray
+        Input data values.
+    X : numpy.ndarray
+        Auxiliary data for regression.
+    D : numpy.ndarray
+        Distance matrix.
+    I : numpy.ndarray
+        Identity matrix.
+    fixed : dict
+        A dictionary of fixed parameters for the covariance model.
+    n_cov : int
+        Number of covariates.
+    names : list
+        Names of parameters to estimate.
+    reml : bool
+        Whether to use Restricted Maximum Likelihood (REML) estimation.
+    
+    Returns
+    -------
+    ll : float
+        Log likelihood value.
+    """
+
+    # Compute covariance matrix
+    if names[-1] == 'sigma_e':  # nugget not fixed
+        Sigma = covf(D, np.exp(p[:-1])) + np.exp(2*p[-1]) * I
+    else:
+        Sigma = covf(D, np.exp(p)) + (fixed['sigma_e'])**2 * I
+
+    # Compute Cholesky factor; if it fails, return a large value
+    R, p = np.linalg.cholesky(Sigma)
     if p > 0:
         ll = np.inf
         return ll
-    
-    SiY = np.linalg.solve(R,np.linalg.solve(R.T,data))
-    
+
+    SiY = np.linalg.solve(R, np.linalg.solve(R.T, data))
+
     if n_cov > 0:
-        SiX = np.linalg.solve(R,np.linalg.solve(R.T,X))
-        v = SiY - SiX.dot(np.linalg.solve(X.T.dot(SiX),X.T.dot(SiY)))
+        SiX = np.linalg.solve(R, np.linalg.solve(R.T, X))
+        v = SiY - SiX.dot(np.linalg.solve(X.T.dot(SiX), X.T.dot(SiY)))
     else:
         v = SiY
-    ll = -np.sum(np.log(np.diag(R))) -0.5*data.T.dot(v)
-    
-    if reml == 1 and n_cov > 0:
-        Rreml,p = np.linalg.cholesky(X.T.dot(SiX))
+
+    ll = -np.sum(np.log(np.diag(R))) - 0.5 * data.T.dot(v)
+
+    if reml and n_cov > 0:
+        Rreml, p = np.linalg.cholesky(X.T.dot(SiX))
         if p > 0:
             ll = np.inf
             return ll
         ll = ll - np.sum(np.log(np.diag(Rreml)))
+
     ll = -ll
     return ll
 
@@ -298,55 +390,86 @@ def init_cov_est(cov, fixed, d_max, s2):
     names = [parameter_names[i] for i in np.where(fixed_pars==0)[0]]
     return p0, names
 
+def cov_ml_est(
+    data: np.ndarray,
+    cov: str,
+    loc: np.ndarray,
+    X: Optional[np.ndarray] = None,
+    fixed: Optional[Dict[str, float]] = None,
+    reml: bool = False
+) -> Dict[str, Union[float, np.ndarray]]:
+    """
+    Estimate covariance parameters using maximum likelihood.
+    
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Input data values.
+    cov : str
+        Covariance model name.
+    loc : numpy.ndarray
+        Locations of the data points.
+    X : numpy.ndarray, optional
+        Auxiliary data for regression.
+    fixed : dict, optional
+        A dictionary of fixed parameters for the covariance model.
+    reml : bool, optional
+        Whether to use Restricted Maximum Likelihood (REML) estimation.
+    
+    Returns
+    -------
+    pars : dict
+        Estimated covariance parameters.
+    """
 
-def cov_ml_est(data, cov, loc, X=None, fixed=None, reml=False):
-        
     if X is None:
         n_cov = 0
     else:
         n_cov = X.shape[1]
-        
+
     if X is None and reml:
         raise ValueError('X must be supplied if REML is used.')
-        
-    #select covariance function
+
+    # Select covariance function
     covf = select_covariance(cov, fixed)
-    
-    #compute distance matrix
+
+    # Compute distance matrix
     D = squareform(pdist(loc))
-    
-    #precompute identiy matrix
+
+    # Precompute identity matrix
     I = np.eye(loc.shape[0])
-    
-    #initial regression estimate of beta
+
+    # Initial regression estimate of beta
     if n_cov > 0:
-        e = data - X.dot(np.linalg.solve(X.T.dot(X),X.T.dot(data)))
+        e = data - X.dot(np.linalg.solve(X.T.dot(X), X.T.dot(data)))
         s2 = np.var(e)
     else:
         s2 = np.var(data)
-    
-    #set initial values for parameters
+
+    # Set initial values for parameters
     p0, names = init_cov_est(cov, fixed, np.max(D), s2)
-    
-    #minimize loss function 
-    par, _, _, _, _, _ = fmin(lambda x: log_like(x, covf, data, X, D, I, fixed, n_cov, names, reml), np.log(p0))
-    par, _, _ = fmin_tnc(lambda x: log_like(x, covf, data, X, D, I, fixed, n_cov, names, reml), par)
-    
-    #extract parameters
+
+    # Define the objective function
+    objective = lambda x: log_like(x, covf, data, X, D, I, fixed, n_cov, names, reml)
+
+    # Use the 'minimize' function with the 'Nelder-Mead' and 'TNC' methods instead of 'fmin' and 'fmin_tnc'
+    res_nm = minimize(objective, np.log(p0), method='Nelder-Mead')
+    res_tnc = minimize(objective, res_nm.x, method='TNC')
+
+    # Extract parameters
     pars = fixed
     for i in range(len(names)):
-        pars[names[i]] = np.exp(par[i])
-    
-    #compute regression coefficients
-    if names[-1] == 'sigma_e': # nugget not fixed
-        Sigma = covf(D,np.exp(par[:-1])) + np.exp(2*par[-1])*I
-    else:
-        Sigma = covf(D,np.exp(par)) + (fixed['sigma_e'])**2*I
-    if n_cov > 0:
-        pars['beta'] = np.linalg.solve(X.T.dot(Sigma).dot(X),X.T.dot(Sigma).dot(data))
-    
-    return pars
+        pars[names[i]] = np.exp(res_tnc.x[i])
 
+    # Compute regression coefficients
+    if names[-1] == 'sigma_e':  # nugget not fixed
+        Sigma = covf(D, np.exp(res_tnc.x[:-1])) + np.exp(2 * res_tnc.x[-1]) * I
+    else:
+        Sigma = covf(D, np.exp(res_tnc.x)) + (fixed['sigma_e'])**2 * I
+    if n_cov > 0:
+        pars['beta'] = np.linalg.solve(X.T.dot(Sigma).dot(X), X.T.dot(Sigma).dot(data))
+
+    return pars
 
 def WLS_loss(p, variogram, fixed, covf, names):
     
@@ -361,33 +484,62 @@ def WLS_loss(p, variogram, fixed, covf, names):
     w = variogram['N']/v**2
 
     #compute loss function
-    S = np.sum(w[variogram['N']>0]*(variogram['variogram'][variogram['N']>0]-v[variogram['N']>0])**2)
+    mask = variogram['N'] > 0
+    S = np.sum(w[mask] * (variogram['variogram'][mask] - v[mask])**2)
     #print("Loss: ", S, "Parameters: ", np.exp(p), "r0: ", r0)
     # write the same print statement above but in matlab code
     
     return S
 
 
-def cov_ls_est(e, cov, variogram, fixed=None):
-        
+def cov_ls_est(
+    e: np.ndarray,
+    cov: str,
+    variogram: Dict[str, np.ndarray],
+    fixed: Optional[Dict[str, float]] = None
+) -> Dict[str, float]:
+    """
+    Estimate covariance parameters using weighted least squares.
+    
+    Parameters
+    ----------
+    e : numpy.ndarray
+        Input data values.
+    cov : str
+        Covariance model name.
+    variogram : dict
+        Dictionary containing the variogram information (bin centers, variogram values, and number of pairs per bin).
+    fixed : dict, optional
+        A dictionary of fixed parameters for the covariance model.
+    
+    Returns
+    -------
+    pars : dict
+        Estimated covariance parameters.
+    """
+
     if fixed is None:
         fixed = {}
     if not isinstance(fixed, dict):
         raise ValueError('fixed should be a dict.')
-        
-    #select covariance function
+
+    # Select covariance function
     covf = select_covariance(cov, fixed)
-    
-    #set initial values for parameters
+
+    # Set initial values for parameters
     p0, names = init_cov_est(cov, fixed, np.max(variogram['h']), np.var(e))
 
-    par = fmin(lambda x: WLS_loss(x, variogram, fixed, covf, names), np.log(p0), maxiter=10000)
+    # Define the objective function
+    objective = lambda x: WLS_loss(x, variogram, fixed, covf, names)
 
-    #extract parameters
+    # Use the 'minimize' function with the 'Nelder-Mead' method instead of 'fmin'
+    res = minimize(objective, np.log(p0), method='Nelder-Mead', options={'maxiter': 10000})
+
+    # Extract parameters
     pars = fixed
     for i in range(len(names)):
-        pars[names[i]] = np.exp(par[i])
-    
+        pars[names[i]] = np.exp(res.x[i])
+
     return pars
 
 
@@ -435,3 +587,8 @@ cm_data = [[0.2081, 0.1663, 0.5292], [0.2116238095, 0.1897809524, 0.5776761905],
  [0.9763, 0.9831, 0.0538]]
 
 parula_map = LinearSegmentedColormap.from_list('parula', cm_data)
+
+
+
+
+
